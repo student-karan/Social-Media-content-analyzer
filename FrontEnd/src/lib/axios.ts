b39b26be8d7f2f4e3f15a03fd80525bd6037c5f7
Jasmeet Singh <karan9310446410@gmail.com>
2026-02-17 12:47:47 +0530

    add backend domain from render

--- FrontEnd/src/lib/axios.ts
+++ FrontEnd/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosIntance = axios.create({
-    baseURL : import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "",
+    baseURL : import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "https://social-media-content-analyzer-o26b.onrender.com/api",
     withCredentials : true
 });
 
