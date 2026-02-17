92d6afdffe9efe26fe0927a6b08ace999eb0a543
Jasmeet Singh <karan9310446410@gmail.com>
2026-02-17 12:43:40 +0530

    fixed some deployment bugs

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -4,6 +4,10 @@ import path from "path";
 import "dotenv/config";
 import { upload } from "./middleware/multer.js";
 import { getAnalysis } from "./controllers/analysis_controller.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT;
--- BackEnd/src/middleware/multer.ts
+++ BackEnd/src/middleware/multer.ts
@@ -1,8 +1,12 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
 
-const uploadDir = path.resolve(__dirname, "../public");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const uploadDir = path.join(__dirname, "../uploads");
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
